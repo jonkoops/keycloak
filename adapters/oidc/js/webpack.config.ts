e78b65989584c80fba47391d49da66336ae7ed0d
Jon Koops <jonkoops@gmail.com>
2020-08-29 19:57:11 +0200

    KEYCLOAK-14817 Allow JS adapter to be bundled as ES module

---
+++ adapters/oidc/js/webpack.config.ts
@@ -0,0 +1,62 @@
+import * as path from 'path'
+import * as TerserPlugin from 'terser-webpack-plugin'
+import { Configuration } from 'webpack'
+
+interface CreateConfigOptions {
+  minimize: boolean
+}
+
+const packagesPath = path.join(__dirname, 'packages')
+const classesPath = path.join(__dirname, 'target/classes')
+
+function createConfig(options: CreateConfigOptions): Configuration {
+  return {
+    mode: 'production',
+    entry: {
+      keycloak: {
+        import: path.join(packagesPath, 'keycloak-js'),
+        library: {
+          type: 'umd',
+          name: 'Keycloak',
+        }
+      },
+      'keycloak-authz': {
+        import: path.join(packagesPath, 'keycloak-authz'),
+        library: {
+          type: 'umd',
+          name: 'KeycloakAuthorization',
+        }
+      },
+    },
+    output: {
+      path: classesPath,
+      filename: options.minimize ? '[name].min.js' : '[name].js',
+    },
+    module: {
+      rules: [{
+        test: /\.(t|j)s$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env'],
+            ]
+          }
+        }
+      }]
+    },
+    devtool: options.minimize ? 'source-map' : false,
+    optimization: {
+      minimize: options.minimize,
+      minimizer: [new TerserPlugin({
+        extractComments: false,
+      })],
+    }
+  }
+}
+
+export default [
+  createConfig({ minimize: false }),
+  createConfig({ minimize: true }),
+]
